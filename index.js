b3e44f9617cc1a580db228c72293b3c7b25f5940
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 13:57:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
+  delete object[key];
   return object
 }
