14b1292115fb5e5fe816831c9df6d3452ecd1574
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 13:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 var recipes = new Object({pasta: "tomatos"});
+
+function updateObjectWithKeyAndValue(object, key, value) {
+
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+}
