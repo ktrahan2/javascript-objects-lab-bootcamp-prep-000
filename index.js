ba033ed340c604d566134b9beff749a48fcdda49
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 13:37:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
-function updateObjectWithKeyandValue(object, key, value) {
-  return object.assign({}, obj, {key: value})
+function updateObjectWithKeyandValue(obj, key, value) {
+  return Object.assign({}, obj, {key: value})
   }
